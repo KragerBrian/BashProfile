8cfefce1314b2a58c5770a20da0d31ccc84a520d
BrianKrager <brian.krager1994@gmail.com>
2024-04-28 20:08:49 -0800

    added new commands

--- profile.sh
+++ profile.sh
@@ -4,5 +4,8 @@ alias refreshBashProfile='source ~/Desktop/Folders/coding-bootcamp-projects/Bash
 alias rbp='refreshBashProfile'
 alias projects='cd ~/Desktop/Folders/coding-bootcamp-projects'
 alias cdbp='cd ~/Desktop/Folders/coding-bootcamp-projects/BashProfile'
+alias flexbox='open ~/Desktop/Folders/Coding_notes/css-flexbox-poster.png'
+alias grid='open ~/Desktop/Folders/Coding_notes/css-grid-poster.png'
+alias notes='open ~/Desktop/Folders/Coding_notes'
 function gagc { git add -A && git commit -m "$1";}
 function gagcgp { gagc "$1" && git push;}
