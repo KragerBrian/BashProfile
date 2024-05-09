930c5a89879f5b2205c37df99f7ab7c72d29b978
BrianKrager <brian.krager1994@gmail.com>
2024-05-08 22:25:43 -0800

    added aliases

--- profile.sh
+++ profile.sh
@@ -3,9 +3,11 @@ alias ebp='editBashProfile'
 alias refreshBashProfile='source ~/Desktop/Folders/coding-bootcamp-projects/BashProfile/profile.sh'
 alias rbp='refreshBashProfile'
 alias projects='cd ~/Desktop/Folders/coding-bootcamp-projects'
+alias aroundtheus='cd ~/Desktop/Folders/coding-bootcamp-projects/se_project_aroundtheus'
 alias cdbp='cd ~/Desktop/Folders/coding-bootcamp-projects/BashProfile'
-alias flexbox='open ~/Desktop/Folders/Coding_notes/css-flexbox-poster.png'
-alias grid='open ~/Desktop/Folders/Coding_notes/css-grid-poster.png'
+alias flexbox='open ~/Desktop/Folders/Coding_notes/HTML_CSS_notes/css-flexbox-poster.png'
+alias grid='open ~/Desktop/Folders/Coding_notes/HTML_CSS_notes/css-grid-poster.png'
 alias notes='open ~/Desktop/Folders/Coding_notes'
+alias javascriptnotes='open ~/Desktop/Folders/Coding_notes/JavaScript_notes'
 function gagc { git add -A && git commit -m "$1";}
 function gagcgp { gagc "$1" && git push;}
